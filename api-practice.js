ce98834c3d90ced07926f209bb0638ee4a11c612
Amanda Stern <amanda.stern.73@gmail.com>
2022-12-21 11:27:44 -0500

    add axios script to html

--- api-practice.js
+++ api-practice.js
@@ -20,17 +20,21 @@ fetch("https://swapi.dev/api/people/1")
     console.log("ERROR!", error)
   })
 
-  const loadStarWarsPeople = async() => {
-    try {
-      const res = await fetch("https://swapi.dev/api/people/1")
-      const data = await res.json()
-      console.log("REQUEST 1", data)
-      const res2 = await fetch("https://swapi.dev/api/people/2")
-      const data2 = await res2.json()
-      console.log("REQUEST 2", data2)
-    } catch (error) {
-      console.log("ERROR!", error)
-    }
+const loadStarWarsPeople = async() => {
+  try {
+    const res = await fetch("https://swapi.dev/api/people/1")
+    const data = await res.json()
+    console.log("REQUEST 1", data)
+    const res2 = await fetch("https://swapi.dev/api/people/2")
+    const data2 = await res2.json()
+    console.log("REQUEST 2", data2)
+  } catch (error) {
+    console.log("ERROR!", error)
   }
+}
 
-  loadStarWarsPeople()
+loadStarWarsPeople()
+
+// AXIOS built on top of javascript...is a library for making HTTP requests
+
+axios.get("https://swapi.dev/api/people/1")
